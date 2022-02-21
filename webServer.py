3e3f51eb1edad35c05ae79ebc9b932e76d34d80c
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2022-02-21 13:07:19 +0100

    remove webserver starting as it is handled by clever ?

--- webServer.py
+++ webServer.py
@@ -18,12 +18,3 @@ class MyServer(BaseHTTPRequestHandler):
 
 
 webServer = HTTPServer((hostName, serverPort), MyServer)
-print("Server started http://%s:%s" % (hostName, serverPort))
-
-try:
-    webServer.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-webServer.server_close()
-print("Server stopped.")
