574fb86676e6d5315de05cdb6046d03343cc2c63
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2022-02-23 18:39:16 +0100

    Revert dev url

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,5 +1,5 @@
 // Clever cloud
-export const apiUrl = "https://api.openloi.fr/api/"
+export const apiUrl = "https://app-9cc3ad75-20bb-4488-bde9-ddd12b8eeea8.cleverapps.io/api/"
 
 // Local
 //export const apiUrl = "http://localhost:5000/api/"
