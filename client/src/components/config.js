29f3493e27fdd3ade2fbb23653cd169331010b22
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2022-02-23 19:11:38 +0100

    Correct api address

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,5 +1,5 @@
 // Clever cloud
-export const apiUrl = "https://app-9cc3ad75-20bb-4488-bde9-ddd12b8eeea8.cleverapps.io/api/"
+export const apiUrl = "https://www.api.openloi.fr/api/"
 
 // Local
 //export const apiUrl = "http://localhost:5000/api/"
