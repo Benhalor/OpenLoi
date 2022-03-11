0821d58171b665cd3191212021be67c4de905dd8
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2022-03-11 16:33:59 +0100

    Message

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import sys
 import time
 
 sys.path.append('AssembleeNationale')
+sys.path.append('Senat')
+import Senat
 import AssembleeNationale
+
+"""senat = Senat.Senat(
+    database='postgres',
+    userDatabase='postgres',
+    passwordDatabase='password',
+    hostDatabase='localhost',
+    portDatabase='5432',
+    verbose=2)
+
+senat.reloadDatabase("Senat/data/")"""
+
 
 if os.getenv("POSTGRESQL_ADDON_DB") is not None:
     # Clever cloud
     assembleeNationale = AssembleeNationale.AssembleeNationale(
         database=os.getenv("POSTGRESQL_ADDON_DB"),
-        userDatabase = os.getenv("POSTGRESQL_ADDON_USER"),
-        passwordDatabase = os.getenv("POSTGRESQL_ADDON_PASSWORD"),
-        hostDatabase = os.getenv("POSTGRESQL_ADDON_HOST"),
-        portDatabase = os.getenv("POSTGRESQL_ADDON_PORT"),
-        verbose=2, 
+        userDatabase=os.getenv("POSTGRESQL_ADDON_USER"),
+        passwordDatabase=os.getenv("POSTGRESQL_ADDON_PASSWORD"),
+        hostDatabase=os.getenv("POSTGRESQL_ADDON_HOST"),
+        portDatabase=os.getenv("POSTGRESQL_ADDON_PORT"),
+        verbose=2,
         setup=False)
 else:
     # Local
@@ -26,22 +39,13 @@ else:
         verbose=2,
         setup=False)
 
-while True:
-    r = assembleeNationale.downloadSources("AssembleeNationale/data/")
-    assembleeNationale.processSources(
-        "AssembleeNationale/data/", r["updatedSources"], replace=True)
-    print("Finished all dump")
-    time.sleep(7200)
+r = assembleeNationale.downloadSources("AssembleeNationale/data/")
+assembleeNationale.processSources(
+    "AssembleeNationale/data/", r["updatedSources"], replace=True)
+print("Finished all dump")
 
-# Tests      
+# Tests
 print(assembleeNationale.search("test"))
 print(assembleeNationale.getDossierLegislatifByUid("DLR5L15N37471"))
 assembleeNationale.getDossierLegislatifdocumentsByUid("PIONANR5L15B3619")
 print(assembleeNationale.getAmendementsByUid("PNREANR5L15B0480"))
-
-"""
-filev = open("/home/gabriel/OpenLoi/AssembleeNationale/data/DOSSIERS_LEGISLATIFS/Dossiers_Legislatifs_XV/json/dossierParlementaire/DLR5L15N36287.json")
-jsonv = json.load(filev)
-ret = assembleeNationale.xtractLastDateDossierParlementaire(jsonv)
-print(ret)
-"""
