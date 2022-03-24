f0fde81a3a31fccf6aeec25e48b5422f6d121f1f
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2022-03-24 11:26:28 +0100

    Link env variable of DB to senat

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ import AssembleeNationale
 sys.path.append('Senat')
 import Senat
 
-senat = Senat.Senat(
-    database='postgres',
-    userDatabase='postgres',
-    passwordDatabase='password',
-    hostDatabase='localhost',
-    portDatabase='5432',
-    verbose=2)
-
-
 
 
 if os.getenv("POSTGRESQL_ADDON_DB") is not None:
@@ -31,6 +22,13 @@ if os.getenv("POSTGRESQL_ADDON_DB") is not None:
         portDatabase=os.getenv("POSTGRESQL_ADDON_PORT"),
         verbose=2,
         setup=False)
+    senat = Senat.Senat(
+        database=os.getenv("POSTGRESQL_ADDON_DB"),
+        userDatabase=os.getenv("POSTGRESQL_ADDON_USER"),
+        passwordDatabase=os.getenv("POSTGRESQL_ADDON_PASSWORD"),
+        hostDatabase=os.getenv("POSTGRESQL_ADDON_HOST"),
+        portDatabase=os.getenv("POSTGRESQL_ADDON_PORT"),
+        verbose=2)
 else:
     # Local
     assembleeNationale = AssembleeNationale.AssembleeNationale(
@@ -42,10 +40,19 @@ else:
         verbose=2,
         setup=False)
 
+    senat = Senat.Senat(
+        database='postgres',
+        userDatabase='postgres',
+        passwordDatabase='password',
+        hostDatabase='localhost',
+        portDatabase='5432',
+        verbose=2)
+
+senat.reloadDatabase("Senat/data/")
+
 r = assembleeNationale.downloadSources("AssembleeNationale/data/")
 assembleeNationale.processSources(
     "AssembleeNationale/data/", r["updatedSources"], replace=True)
 
-senat.reloadDatabase("Senat/data/")
 
 print("Finished all dump")
